72c4b9857f79f4794bd99b454350a12cdab98516
Rahul <rahulsrivastava503@gmail.com>
2025-05-17 11:49:29 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     strictPort: false,
+    allowedHosts: ['intuteai.up.railway.app'], // Add the host here
     proxy: {
       '/api': {
         target: 'https://crmbackend-production-c426.up.railway.app',
